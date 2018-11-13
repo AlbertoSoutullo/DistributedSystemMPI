67f44bd53b1e94e027cff2aeceea54e9477ac53f
Alberto <alberto.soutullo@hotmail.com>
2018-11-13 08:59:11 -0800

    Changes to print, subir

---
+++ harddisk.cpp
@@ -0,0 +1,6 @@
+#include "harddisk.h"
+
+HardDisk::HardDisk()
+{
+
+}
---
+++ harddisk.h
@@ -0,0 +1,11 @@
+#ifndef HARDDISK_H
+#define HARDDISK_H
+
+
+class HardDisk
+{
+public:
+    HardDisk();
+};
+
+#endif // HARDDISK_H
