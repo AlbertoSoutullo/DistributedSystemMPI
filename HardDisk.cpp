1bc7a19f6856b4842a6e0b88b61e1803caaf4118
Alberto <alberto.soutullo@hotmail.com>
2018-12-10 08:03:14 -0800

    download works

--- HardDisk.cpp
+++ HardDisk.cpp
@@ -89,7 +89,7 @@ void HardDisk::writeBlock(char* data, int HDD, int block)
     std::string fileName = "disk" + std::to_string(HDD) + ".dat";
     std::ofstream binaryFile;
 
-    binaryFile.open(fileName, std::ios::out | std::ios::binary);
+    binaryFile.open(fileName, std::ios::out | std::ios::binary | std::ios::in);
     binaryFile.seekp(BLOCK_SIZE*block);
     binaryFile.write((char*)data, sizeof(char)*BLOCK_SIZE);
     binaryFile.close();
@@ -117,12 +117,21 @@ void HardDisk::overrideSectors()
 /*Subir*/
 void HardDisk::writeFile(Node* fileNode)
 {
+    int restSize = -1;
+    bool flagRest = false;
     std::string string_cwd = std::string(this->cwd);
 
     //dividir el archivo en bloques (restante a cero)
     off_t fileSize = fileNode->getByteSize();
     const off_t blockSize = BLOCK_SIZE;
-    int numberOfBlocks = (fileSize / blockSize) + 1;
+    int numberOfBlocks = fileSize / blockSize;
+
+    if ((fileSize % blockSize) != 0)
+    {
+        restSize = fileSize - (numberOfBlocks*BLOCK_SIZE);
+        flagRest = true;
+    }
+
     int pos = 0; //Last position of reader
 
     //en un for, por cada bloque leer esa parte, mirar que disco está vacio, escribir
@@ -134,8 +143,8 @@ void HardDisk::writeFile(Node* fileNode)
         fs.open(string_cwd + "/" + fileNode->getName(), std::ios::in | std::ios::binary);
         if (!fs.is_open()) std::cout << "Cannot open file." << std::endl;
         fs.seekg(pos, fs.beg);
-        fs.read((char*)binaryData, sizeof(binaryData));
-        pos = fs.tellg();
+        fs.read((char*)binaryData, sizeof(char)*BLOCK_SIZE);
+        pos += BLOCK_SIZE;
 
         int HDD = getEmptyHdd();
 
@@ -157,6 +166,38 @@ void HardDisk::writeFile(Node* fileNode)
             //free(binaryData);
         }
     }
+    if(flagRest)
+    {
+        std::ifstream fs;
+        char* binaryData = (char*)malloc(sizeof(char)*restSize);
+        //Open File and Check size
+        fs.open(string_cwd + "/" + fileNode->getName(), std::ios::in | std::ios::binary);
+        if (!fs.is_open()) std::cout << "Cannot open file." << std::endl;
+        fs.seekg(pos, fs.beg);
+        fs.read((char*)binaryData, sizeof(char)*restSize);
+        pos = fs.tellg();
+
+        int HDD = getEmptyHdd();
+
+        if (HDD == -1)
+        {
+            std::cout << "Disks ran out of space." << std::endl;
+            fs.close();
+        }
+        else
+        {
+            int block = getBlock(HDD);
+
+            writeBlock(binaryData, HDD, block);
+            //Escribir en nodo
+            (*fileNode).setBlock(block, HDD);
+
+            fs.close();
+            //free(binaryData);
+        }
+    }
+
+
     //Actualizar el fichero de sectores
     (*fileNode).setNumBlocksOccupied();
     overrideSectors();
@@ -167,8 +208,9 @@ void HardDisk::readBlock(std::ifstream &disk, std::ofstream &file, int position,
 {
     char* binaryData = (char*)malloc(sizeof(char)*BLOCK_SIZE);
     disk.seekg(block*BLOCK_SIZE);
-    disk.read((char*)binaryData, sizeof(binaryData));
+    disk.read((char*)binaryData, sizeof(char)*BLOCK_SIZE);
 
+    //sacar
     file.seekp(BLOCK_SIZE*position);
     file.write((char*)binaryData, sizeof(char)*BLOCK_SIZE);
     //free(binaryData);
@@ -182,7 +224,9 @@ void HardDisk::readFile(Node* fileNode)
     std::string string_cwd = std::string(this->cwd);
     string_cwd += "/";
     string_cwd += fileNode->getName();
-    file.open(string_cwd, std::ios::in | std::ios::binary | std::ios::trunc);
+    file.open(string_cwd, std::ios::out | std::ios::binary | std::ios::trunc);
+
+    string_cwd = std::string(this->cwd);
 
     std::vector<location_t> locations = fileNode->getBlockLocations();
 
